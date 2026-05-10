fedc2c6c10d4d78d366fa4794449225f21f033f9
takuya <14888534+takuyash@users.noreply.github.com>
2026-05-10 14:19:54 +0900

    バージョン情報の更新

--- WinSimpleFolderLauncher/HelpForm.cs
+++ WinSimpleFolderLauncher/HelpForm.cs
@@ -13,8 +13,11 @@ namespace StylishLauncherINI
     {
 
         private const string GitHubRepoUrl = "https://github.com/takuyash/WinSimpleFolderLauncher";
-        private const string HelpUrl = "https://takuyash.github.io/WinSimpleFolderLauncherSite/docs.html";
+
+        private const string HelpUrl = "";
+
         private const string LicenseUrl = "https://github.com/takuyash/WinSimpleFolderLauncher/blob/main/LICENSE";
+
         private Label _updateLabel;
         private FlowLayoutPanel panel;
 
